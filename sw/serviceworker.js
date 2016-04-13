68de6029b898fb4cf773ef197f2c01d3fa51e426
yongling8808 <522919201@qq.com>
2016-04-13 10:09:29 +0800

    1

--- sw/serviceworker.js
+++ sw/serviceworker.js
@@ -1,35 +1,18 @@
+importScripts('serviceworker-cache-polyfill.js');
 
 const OFFLINE_CACHE = 'v5';
 
 this.addEventListener('install', function(event) {
 
-console.log('serviceworker thread oninstall function');
-
-const offlineRequest = new Request('index.html');
-console.log('serviceworker thread oninstall function01');
-  // event.waitUntil(
-    fetch(offlineRequest).then(function(response) {
-      console.log('serviceworker thread oninstall function02');
-      return caches.open(OFFLINE_CACHE).then(function(cache) {
-        console.log('serviceworker thread oninstall function03');
-        return cache.put(offlineRequest, response);
-      });
-    });
-  // );
-
-const ImageRequest = new Request('snowTroopers.jpg');
-fetch(ImageRequest).then(function(response) {
-  return caches.open(OFFLINE_CACHE).then(function(cache) {
-    return cache.put(ImageRequest, response);
-  });
-});
-
-const JsRequest = new Request('app.js');
-fetch(JsRequest).then(function(response) {
-  return caches.open(OFFLINE_CACHE).then(function(cache) {
-    return cache.put(JsRequest, response);
-  });
-});
+event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/sw/sw/index.html',
+        '/sw/sw/snowTroopers.jpg',
+        '/sw/sw/app.js',
+      ]);
+    })
+  );
                  
 });
 

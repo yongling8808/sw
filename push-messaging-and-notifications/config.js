83ff2c411f8db3b8b82af59b551ef32b175c298a
yongling8808 <522919201@qq.com>
2016-04-20 10:55:47 +0800

    2

--- push-messaging-and-notifications/config.js
+++ push-messaging-and-notifications/config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyDwu52HktvptWIKBS_3-8c1JKKXh8FBcdg'
+  gcmAPIKey: 'AIzaSyBKzYswiNYemLu-58ZJWY7ZajXHIgyjatM'
 };

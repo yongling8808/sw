507ad77ffb3012142196a012491399d1ab201ecf
yongling8808 <522919201@qq.com>
2016-05-13 15:32:37 +0800

    1

---
+++ analysis/serviceworker.js
@@ -0,0 +1,139 @@
+//importScripts('serviceworker-cache-polyfill.js');
+
+if (!Cache.prototype.add) {
+  Cache.prototype.add = function add(request) {
+    return this.addAll([request]);
+  };
+}
+
+if (!Cache.prototype.addAll) {
+  Cache.prototype.addAll = function addAll(requests) {
+    var cache = this;
+
+    // Since DOMExceptions are not constructable:
+    function NetworkError(message) {
+      this.name = 'NetworkError';
+      this.code = 19;
+      this.message = message;
+    }
+    NetworkError.prototype = Object.create(Error.prototype);
+
+    return Promise.resolve().then(function() {
+      if (arguments.length < 1) throw new TypeError();
+      
+      // Simulate sequence<(Request or USVString)> binding:
+      var sequence = [];
+
+      requests = requests.map(function(request) {
+        if (request instanceof Request) {
+          return request;
+        }
+        else {
+          return String(request); // may throw TypeError
+        }
+      });
+
+      return Promise.all(
+        requests.map(function(request) {
+          if (typeof request === 'string') {
+            request = new Request(request);
+          }
+
+          var scheme = new URL(request.url).protocol;
+
+          if (scheme !== 'http:' && scheme !== 'https:') {
+            throw new NetworkError("Invalid scheme");
+          }
+
+          return fetch(request.clone());
+        })
+      );
+    }).then(function(responses) {
+      // TODO: check that requests don't overwrite one another
+      // (don't think this is possible to polyfill due to opaque responses)
+      return Promise.all(
+        responses.map(function(response, i) {
+          return cache.put(requests[i], response);
+        })
+      );
+    }).then(function() {
+      return undefined;
+    });
+  };
+}
+
+if (!CacheStorage.prototype.match) {
+  // This is probably vulnerable to race conditions (removing caches etc)
+  CacheStorage.prototype.match = function match(request, opts) {
+    var caches = this;
+
+    return this.keys().then(function(cacheNames) {
+      var match;
+
+      return cacheNames.reduce(function(chain, cacheName) {
+        return chain.then(function() {
+          return match || caches.open(cacheName).then(function(cache) {
+		     
+			 if(opts) {
+			    return cache.match(request, opts);
+			 } else {
+			    return cache.match(request);
+			 }
+            
+          }).then(function(response) {
+            match = response;
+            return match;
+          });
+        });
+      }, Promise.resolve());
+    });
+  };
+}
+ 
+const OFFLINE_CACHE = 'v5';
+
+this.addEventListener('install', function(event) {
+
+event.waitUntil(
+    caches.open(OFFLINE_CACHE).then(function(cache) {
+      return cache.addAll([
+        '/sw/analysis/index.html',
+        // '/sw/analysis/snowTroopers.jpg',
+        '/sw/analysis/app.js',
+      ]);
+    })
+  );
+          
+});
+ 
+
+this.addEventListener('activate', function(event) {
+    console.log('serviceworker activate');
+});
+   
+ 
+this.addEventListener('fetch', function(event)  {
+
+
+  // self.clients.getAll().then(function(clients) {
+  //     clients.forEach(function(client) {
+  //         console.log('postMessage');
+  //         client.postMessage('serviceworker-update');
+  //     });
+  // });  
+
+ 
+  console.log('serviceworker thread onfetch function');
+  console.log('this comes frome server!');
+  console.log('Handling fetch event for '+ event.request.url);
+  event.respondWith(
+        caches.open(OFFLINE_CACHE).then(function(cache) {
+          console.log('load from cache:'+ event.request.url);
+          return cache.match(event.request.url);
+        }).catch(function(error) {
+            var req = new Request('/sw/analysis/snowTroopers.jpg');
+            return fetch(request.clone());
+        })
+    ); 
+  console.log('Handling fetch event end!');
+}); 

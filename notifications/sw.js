075e37dbe9d24fd0dd9a61dad827993d5d340977
yongling8808 <522919201@qq.com>
2016-06-20 13:01:08 +0800

    2

--- notifications/sw.js
+++ notifications/sw.js
@@ -16,14 +16,15 @@ self.addEventListener('notificationclick', function(event) {
 //Utility function to handle the click
 function handleNotificationClick(evt) {
     evt.notification.close();
-    var iconURL = evt.notification.icon;
-    if (iconURL.indexOf("?") > -1) {
-        var queryString = iconURL.split("?")[1];
-        var query = parseQueryString(queryString);
-        if (query.url && query.url.length == 1) {
-            if (_roostSW.logging) console.log("Opening URL: " + query.url[0]);
-            return clients.openWindow(query.url[0]);
-        }
-    }
+//    var iconURL = evt.notification.icon;
+//    if (iconURL.indexOf("?") > -1) {
+//        var queryString = iconURL.split("?")[1];
+//        var query = parseQueryString(queryString);
+//        if (query.url && query.url.length == 1) {
+//            if (_roostSW.logging) console.log("Opening URL: " + query.url[0]);
+//            return clients.openWindow(query.url[0]);
+//        }
+//    }
+clients.openWindow('https://yongling8808.github.io/sw/notifications/requireInteraction.html');
     console.log("Failed to redirect to notification for iconURL: " + iconURL);
 }

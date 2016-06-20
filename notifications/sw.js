e1c6ddfec842ce9de25bcab38e73a2d542cb6032
yongling8808 <522919201@qq.com>
2016-06-20 12:34:18 +0800

    1

--- notifications/sw.js
+++ notifications/sw.js
@@ -8,5 +8,23 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
   // Close the notification when it is clicked
-  event.notification.close();
+  //event.notification.close();
+  event.waitUntil(handleNotificationClick(event));
 });
+
+
+//Utility function to handle the click
+function handleNotificationClick(evt) {
+    if (_roostSW.logging) console.log("Notification clicked: ", evt.notification);
+    evt.notification.close();
+    var iconURL = evt.notification.icon;
+    if (iconURL.indexOf("?") > -1) {
+        var queryString = iconURL.split("?")[1];
+        var query = parseQueryString(queryString);
+        if (query.url && query.url.length == 1) {
+            if (_roostSW.logging) console.log("Opening URL: " + query.url[0]);
+            return clients.openWindow(query.url[0]);
+        }
+    }
+    console.log("Failed to redirect to notification for iconURL: " + iconURL);
+}

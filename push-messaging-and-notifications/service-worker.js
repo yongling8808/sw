9bdd67d17d93b7b20357dcefcd275432984c9710
yongling8808 <522919201@qq.com>
2016-07-19 14:09:57 +0800

    1

--- push-messaging-and-notifications/service-worker.js
+++ push-messaging-and-notifications/service-worker.js
@@ -1,7 +1,7 @@
 //'use strict';
 
 self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
+  console.log('Received a push message');
 
   var title = 'Yay a message.';
   var body = 'We have received a push message.';
@@ -19,7 +19,7 @@ self.addEventListener('push', function(event) {
 
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
+  console.log('On notification click: ' + event.notification.tag);
   // Android doesn’t close the notification when you click on it
   // See: http://crbug.com/463146
   event.notification.close();

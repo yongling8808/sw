011b2e58b307262bf1a6a49004a3c16d38f923ef
yongling8808 <522919201@qq.com>
2016-11-29 12:33:28 +0800

    2

---
+++ bug/serviceworker.js
@@ -0,0 +1,58 @@
+
+var config = {
+    db : 'db_20161129123200'
+};
+
+var addToCache = function(req) {
+    
+    return fetch(req.clone()).then(function(resp) {
+        
+        var cacheResp = resp.clone();
+        if (resp.status !== 200 || (resp.type !== 'basic' && resp.type !== 'cors')) {
+            return resp;
+        }
+        caches.open(config.db).then(function(cache) {
+            cache.put(req.clone(), cacheResp);
+        });
+        return resp;
+    });
+};
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(cacheNames) {
+        return Promise.all(cacheNames.map(function(cacheName) {
+            if (cacheName !== config.db) {
+                return caches.delete(cacheName);
+            }
+        }));
+    }));
+});
+
+self.addEventListener('fetch', function(event) {
+    
+    var promise, req, url = event.request.url;
+    
+    if (url.indexOf('jsreport') !== -1 || url.indexOf('bypass=1') !== -1 || url.indexOf('http:') === 0) {
+        event.respondWith(fetch(event.request.clone()));
+        return;
+    }
+
+    if (url.indexOf('cors=1') !== -1) {
+        req = new Request(url, {mode : 'cors'});
+    } else {
+        req = event.request.clone();
+    }
+    
+    promise = caches.open(config.db).then(function(cache) {
+        return cache.match(req);
+    }).then(function(response) {
+        if (response) {
+            return response;
+        } else {
+            return addToCache(req);
+        }
+    });
+    
+    event.respondWith(promise);
+    
+});

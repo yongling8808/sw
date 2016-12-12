8c7c60bb1791a1cd492d6c03a59f183dc40b4dae
yongling8808 <522919201@qq.com>
2016-12-12 16:01:59 +0800

    1

--- bug/serviceworker.js
+++ bug/serviceworker.js
@@ -819,12 +819,12 @@ routes.add(route);
 
 
 self.addEventListener('install', function(event) {
-    installListener(event, options);
+    //installListener(event, options);
 });
 self.addEventListener('activate', function(event) {
-    activateListener(event, options);
+    //activateListener(event, options);
 });
 self.addEventListener('fetch', function(event) {
-    fetchListener(event, routes);
+    //fetchListener(event, routes);
 });
 /**********************************************************************************/

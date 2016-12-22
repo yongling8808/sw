9004ffeea3943f6c777891ca026756c33de9ae74
yongling8808 <522919201@qq.com>
2016-12-22 18:30:15 +0800

    1

--- performance/js/report.js
+++ performance/js/report.js
@@ -1,5 +1,6 @@
 function performanceReport() {
 	var cost = performance.timing.loadEventEnd - performance.timing.fetchStart;
-	
+	console.log(performance.timing.loadEventEnd);
+	console.log(performance.timing.fetchStart);
 	console.log('resource_load_time_cost:' + cost);
 }

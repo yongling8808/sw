b3e19f0b0fd78ce25861fc4cb4dd20fa9f649045
yongling8808 <522919201@qq.com>
2016-12-23 14:56:25 +0800

    1

--- performance/js/report.js
+++ performance/js/report.js
@@ -1,6 +1,13 @@
 function performanceReport() {
 	var cost = performance.timing.loadEventEnd - performance.timing.fetchStart;
-	console.log(performance.timing.loadEventEnd);
-	console.log(performance.timing.fetchStart);
 	console.log('resource_load_time_cost:' + cost);
-}
+	if(sessionStorage.getItem("reported")==="true")
+	{
+		console.log('you refresh the page, do not report');
+	}
+	else
+	{
+		sessionStorage.setItem("reported","true");
+		console.log('you first open the page!');
+	}
+} 

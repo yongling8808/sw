63fb0bcbd8c1e7da4b641c0212b4c28d6254ead7
yongling8808 <522919201@qq.com>
2017-03-25 20:34:23 +0800

    2

--- samples/service-worker/offline-page/service-worker.js
+++ samples/service-worker/offline-page/service-worker.js
@@ -1,6 +1,6 @@
 //cachestorage名称，可以加上版本号予以区分
 const OFFLINE_CACHE_PREFIX = 'offline_page_';
-const CACHE_VERSION = 'v1.0';
+const CACHE_VERSION = 'v1.1';
 const OFFLINE_CACHE_NAME = OFFLINE_CACHE_PREFIX + CACHE_VERSION;
 
 //Service Worker安装事件，其中可以预缓存资源
@@ -22,18 +22,18 @@ this.addEventListener('install', function(event) {
 });
  
 //Service Worker激活事件
-this.addEventListener('activate', function(event) {
-  //在激活事件中清除非当前版本的缓存避免用户存储空间急剧膨胀
-  event.waitUntil(caches.keys().then(function(cacheNames) {
-    return Promise.all(cacheNames.map(function(cacheName) {
-        if (cacheName !== OFFLINE_CACHE_NAME) {
-          if(cacheName.indexOf(OFFLINE_CACHE_PREFIX) != -1) {
-            return caches.delete(cacheName);
-          }
-        }
-    }));
-  }));
-});
+// this.addEventListener('activate', function(event) {
+//   //在激活事件中清除非当前版本的缓存避免用户存储空间急剧膨胀
+//   event.waitUntil(caches.keys().then(function(cacheNames) {
+//     return Promise.all(cacheNames.map(function(cacheName) {
+//         if (cacheName !== OFFLINE_CACHE_NAME) {
+//           if(cacheName.indexOf(OFFLINE_CACHE_PREFIX) != -1) {
+//             return caches.delete(cacheName);
+//           }
+//         }
+//     }));
+//   }));
+// });
    
 
 //Service Worker 请求拦截事件

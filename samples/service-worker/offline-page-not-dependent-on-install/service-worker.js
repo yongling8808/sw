8a9dec99ab21b3d6860a225b31a87fd905947db5
yongling8808 <522919201@qq.com>
2017-03-25 20:20:08 +0800

    2

--- samples/service-worker/offline-page-not-dependent-on-install/service-worker.js
+++ samples/service-worker/offline-page-not-dependent-on-install/service-worker.js
@@ -4,11 +4,12 @@ const OFFLINE_CACHE_NAME = 'offline_page_not_dependent_on_install_cache_name_v1.
 //Service Worker安装事件，其中可以预缓存资源
 this.addEventListener('install', function(event) {
   
-  //需要缓存的页面资源
+  //需要缓存的重要的高优先级资源
   var vipUrlsToPrefetch = [
     './index.html',
   ];
 
+  //次重要的资源
   var urlsToPrefetch = [
     './images/banner.png',
     './css/main.css',
@@ -17,7 +18,9 @@ this.addEventListener('install', function(event) {
 
   event.waitUntil(
     caches.open(OFFLINE_CACHE_NAME).then(function(cache) {
+      //urlsToPrefetch非重要资源，即使有资源加载失败也不影响Service Worker安装
       cache.addAll(urlsToPrefetch);
+      //vipUrlsToPrefetch中资源全部请求成功，Service Worker安装事件才顺利完成，可以进入激活事件
       return cache.addAll(vipUrlsToPrefetch);
     })
   );      
